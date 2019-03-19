0ae469a5846a66a5ad6490685c5ed4cc53937a0f
Saint-Jean Germila <germila@noukod.com>
2019-03-19 11:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function BoardMember(name, homeState, training)
 {
-  this.name = name
-  this.homeState = homeState
-  this.training = training
-  this.sayHi = function(){
-    console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and  I was training in ${this.training}.`)
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+
 
     this.veto = function(){ return 'No, I must disagree'};
     this.approve  = function(){ return 'You can do that!'};
