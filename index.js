163a03d2be2e689b04046bfebba93368e6bd3606
Saint-Jean Germila <germila@noukod.com>
2019-03-19 11:35:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ function BoardMember(name, homeState, training)
   this.training = training
   this.sayHi = function(){
     console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and  I was training in ${this.training}.`)
+
+    this.veto = function(){ return 'No, I must disagree'};
+    this.approve  = function(){ return 'You can do that!'};
+    this.doCharity  = function(){ return 'I like to help people.'};
+    this.releasePressStatement  = function(){ return 'You will see great things from Scuber.'};
+    this.sayHi = function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`};
   }
 }
 
-function veto()
-{
-  console.log("No, Imust disagree")
-}
