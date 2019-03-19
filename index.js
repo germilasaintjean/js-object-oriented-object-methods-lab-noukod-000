7e344615ffaa1eccd6257aa62ed4ba4ab2826993
Saint-Jean Germila <germila@noukod.com>
2019-03-19 11:27:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function BoardMember(name, homeState, training)
+{
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  this.sayHi = function(){
+    console.log(`Hi, my name is ${name}. I am from ${homeState}, and  I was training in ${training}.`)
+  }
+}
